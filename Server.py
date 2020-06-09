eaec18e2287876f2381f7baffdc17abd9cafc205
Hei XiaoBai <heixiaobai@hotmail.com>
2020-06-09 15:51:50 +0800

    sava option

--- Server.py
+++ Server.py
@@ -11,9 +11,17 @@ def index():
     return "Test"
 
 
-@app.route("/api/auth", methods=['POST'])
+@app.route("/api/option", methods=['POST'])
 def auth():
-    if fun.authCookie(request.form.get('Cookie')):
+    # print(request.form.get('Cookie'))
+    cookie = request.form.get('Cookie')
+    print(type(request.form.get('Cookie')))
+
+    # 验证传递的cookie
+    if fun.auth_cookie(cookie):
+        uid = dict(i.split("=") for i in cookie.split("; "))['auth_key']
+        options = request.form.get('options_data')
+        fun.save_option(uid, options)
         return "Yes"
     else:
-        return "No"
+        return "Auth Error"
--- fun.py
+++ fun.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
+import redis
 import requests
 from requests.adapters import HTTPAdapter
 
 
-def authCookie(cookie: str):
+def save_option(uid: str, options):
+    r = redis.StrictRedis(host="localhost", port=6379)
+    r.set(uid, options)
+
+
+def auth_cookie(cookie: str):
     """
     传入Cookie，向官方API验证
     :param cookie: Cookie
@@ -13,12 +19,15 @@ def authCookie(cookie: str):
     header = {
         "User-Agent":
             "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2954.36 Safari/537.36",
-        "Cookies": cookie
+        "Cookie": cookie
     }
+
     s = requests.session()
     s.mount('https://', HTTPAdapter(max_retries=2))
     try:
+        # print(header)
         r = requests.get(url=url, headers=header, timeout=(5, 5))
+        print(r.json())
         if r.json()['result'] == 0:
             return True
         else:
