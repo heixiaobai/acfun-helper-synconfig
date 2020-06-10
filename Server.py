83e1f7ef2008e26b339d412ff0fb56fcee69e608
Hei XiaoBai <heixiaobai@hotmail.com>
2020-06-10 14:59:56 +0800

    增加大量注释

--- Server.py
+++ Server.py
@@ -9,29 +9,33 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Test"
+    return "AcFun 辅助站点"
 
 
 @app.route("/api/acfun-helper/options/version", methods=['GET'])
 def option_version():
+    """
+    接口修改后版本号变更
+    :return:版本号
+    """
     version_info = "bate-0.2"
     return version_info
 
 
 @app.route("/api/acfun-helper/options/upload", methods=['POST'])
 def upload():
-    # print(request.form.get('options_data'))
-
+    """
+    上传接口，接收form-data格式的options
+    :return: 返回确认或错误内容，对于未约定的出错情况，前端应该直接输出返回值
+    """
     optionsJson = json.loads(request.form.get('options_data'))
+    # 从options中读取cookie，因为AcPassToken被放在另一个key，所以需要拼接
     cookie = optionsJson["AcCookies"]+"; acPasstoken="+optionsJson["AcPassToken"]
-    # print(cookie)
-    # print(type(request.form.get('Cookie')))
 
-    # 验证传递的cookie
+    # 验证传递的cookie，验证正常则根据uid将options存入redis
     if fun.auth_cookie(cookie):
         uid = dict(i.split("=") for i in cookie.split("; "))['auth_key']
         optionsJson.pop("AcCookies")
-        # optionsJson.pop("AcpushList1")
         fun.save_option(uid, json.dumps(optionsJson))
         return "OK"
     else:
@@ -40,6 +44,10 @@ def upload():
 
 @app.route("/api/acfun-helper/options/download", methods=['POST'])
 def download():
+    """
+    下载接口，接收form-data格式，存在cookie和AcPassToken的dict
+    :return: 返回options，UID对应的option不存在返回None
+    """
     authCookie = json.loads(request.form.get('authCookie'))
     cookie = authCookie["AcCookies"]+"; acPasstoken="+authCookie["AcPassToken"]
     if fun.auth_cookie(cookie):
--- fun.py
+++ fun.py
@@ -3,25 +3,37 @@ import redis
 import requests
 from requests.adapters import HTTPAdapter
 
+# TODO: 临时连接测试用，上线时应该重新修改
 r = redis.StrictRedis(host="localhost", port=6379)
 
 
-def save_option(uid: str, options):
+def save_option(uid: str, options: str):
+    """
+    根据UID将options存入Redis
+    :param uid: uid
+    :param options: 字符化的dict
+    :return: None
+    """
     r.set(uid, options)
-    print("test:oK")
 
 
 def load_option(uid: str):
+    """
+    从Redis读取UID对应的options并返回
+    :param uid: UId
+    :return: 字符化的dict
+    """
     return r.get(uid)
 
 
 def auth_cookie(cookie: str):
     """
-    传入Cookie，向官方API验证
+    传入Cookie，向AcFun官方API验证用户
     :param cookie: Cookie
-    :return: True or False
+    :return: BOOL True or False
     """
     url = "https://api-new.app.acfun.cn/rest/app/user/hasSignedIn"
+    # 需要定义header，否则会返回403
     header = {
         "User-Agent":
             "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2954.36 Safari/537.36",
@@ -29,16 +41,19 @@ def auth_cookie(cookie: str):
     }
 
     s = requests.session()
+    # 超时重试2次
     s.mount('https://', HTTPAdapter(max_retries=2))
     try:
-        # print(header)
-        r = requests.get(url=url, headers=header, timeout=(5, 5))
-        print(r.json())
-        if r.json()['result'] == 0:
+        # 超时时间和读取超时均为5秒
+        res = requests.get(url=url, headers=header, timeout=(5, 5))
+        # 返回json中，result为0是正常用户，否则就是cookie有误
+        if res.json()['result'] == 0:
             return True
         else:
             return False
     except Exception as e:
+        # 暂时没有遇到需要处理的错误
+        # TODO: 应该将错误写入error log，而不是答应
         print(e.args)
         import traceback
         print(traceback.format_exc())
